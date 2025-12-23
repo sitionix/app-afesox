a100df0379937080eb08ed32f21ce67ed4d41cf7
vladvinskevitch <vvinskevitch@gmail.com>
2025-12-23 21:53:20 +0200

    [SITIONIX-50] - add avro definition

---
+++ src/main/resources/event-templates/producer/EventProducer.java
@@ -0,0 +1,27 @@
+package ${event.wrapper.package};
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import ${event.package}.${event.class};
+
+public class ${event.class}Producer implements AutoCloseable {
+    private final KafkaProducer<String, ${event.class}> producer;
+    private final String topic;
+
+    public ${event.class}Producer(Properties properties, Serializer<${event.class}> valueSerializer, String topic) {
+        this.producer = new KafkaProducer<>(properties, new StringSerializer(), valueSerializer);
+        this.topic = topic;
+    }
+
+    public void send(String key, ${event.class} value) {
+        producer.send(new ProducerRecord<>(topic, key, value));
+    }
+
+    @Override
+    public void close() {
+        producer.close();
+    }
+}
